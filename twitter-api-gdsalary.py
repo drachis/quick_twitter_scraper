c274517665ddbc2c809b01ca8e5e87023f542a56
Toli Carter <drachis@drachis.com>
2020-06-07 13:59:21 -0700

    created simple scraper,
    filtered out api-keys.py,
    based on tutorial https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/twitter-data-in-python/
    require twitter developer api access https://developer.twitter.com/

---
+++ twitter-api-gdsalary.py
@@ -0,0 +1,31 @@
+import os
+import tweepy as tw
+import pandas as pd
+import api_keys 
+consumer_key = api_keys.consumer_key
+consumer_secret = api_keys.consumer_secret
+access_token = api_keys.access_token
+access_token_secret= api_keys.access_token_secret
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+search_words = "#gamedevpaidme"
+new_search = search_words + " -filter:retweets"
+date_since = "2020-05-30"
+
+tweets = tw.Cursor(api.search, 
+                   q=new_search,
+                   lang="en", 
+                   since=date_since,
+                   tweet_mode="extended").items(1500)
+info = [[tweet.user.screen_name, tweet.user.location, tweet.full_text ] for tweet in tweets]
+tweet_data = pd.DataFrame(data = info,
+              columns=[
+                  "user", 
+                  "location" , 
+                  "text"
+                       ])
+with open("tweets.csv", mode='w') as file:
+    file.write(tweet_data.to_csv(encoding='utf-8'))
