a73025af34524a779a72f17aae44c146d5e08530
Toli Carter <drachis@drachis.com>
2020-06-07 18:47:49 -0700

    pulled animation pay as well.

--- twitter-api-gdsalary.py
+++ twitter-api-gdsalary.py
@@ -11,7 +11,7 @@ auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
-search_words = "#gamedevpaidme"
+search_words = "#animationpaidme"
 new_search = search_words + " -filter:retweets"
 date_since = "2020-05-30"
 
@@ -19,7 +19,7 @@ tweets = tw.Cursor(api.search,
                    q=new_search,
                    lang="en", 
                    since=date_since,
-                   tweet_mode="extended").items(1500)
+                   tweet_mode="extended").items(1200)
 info = [[tweet.user.screen_name, tweet.user.location, tweet.full_text ] for tweet in tweets]
 tweet_data = pd.DataFrame(data = info,
               columns=[
@@ -27,5 +27,5 @@ tweet_data = pd.DataFrame(data = info,
                   "location" , 
                   "text"
                        ])
-with open("tweets.csv", mode='w') as file:
+with open("animation_pay.csv", mode='w') as file:
     file.write(tweet_data.to_csv(encoding='utf-8'))
